381f6e830365398fc2f08e3291accd792fe202b1
artuur <arturcastrodossantos.com@gmail.com>
2025-02-20 13:32:47 -0300

    teste unitário de delete da question

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,36 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import { DeleteQuestionUseCase } from "./delete-question"
+import { InMemoryQuestionRepository } from "../../../../../test/repositories/in-memory-questions-repository"
+import { makeQuestion } from "../../../../../test/factories/make-question"
+
+describe('Delete question', () => {
+    
+    let inMemoryQuestionRepository: InMemoryQuestionRepository
+    let sut: DeleteQuestionUseCase
+
+    beforeEach(()=>{
+
+        inMemoryQuestionRepository = new InMemoryQuestionRepository()
+        sut = new DeleteQuestionUseCase(inMemoryQuestionRepository)
+
+    })
+
+
+    it('Find question by slug', async () => {
+
+        const newQuestion = makeQuestion({
+            title: 'primeira-question'
+        })
+        inMemoryQuestionRepository.create(newQuestion)
+
+        console.log(inMemoryQuestionRepository.items)
+
+       await sut.execute({
+            id: newQuestion.id.valueId
+        })
+        
+        expect(inMemoryQuestionRepository.items).length(0)
+    
+    })
+
+})
