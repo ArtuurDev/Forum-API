84ebd09fee123898fe58e4b0d00b2f02aa4e84b3
artuur <arturcastrodossantos.com@gmail.com>
2025-02-20 13:33:16 -0300

    teste de get da question por slug

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,38 @@
+import {beforeEach, describe, expect, it} from 'vitest'
+import { InMemoryQuestionRepository } from '../../../../../test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { makeQuestion } from '../../../../../test/factories/make-question'
+
+describe('Create question', () => {
+    
+    let inMemoryQuestionRepository: InMemoryQuestionRepository
+    let sut: GetQuestionBySlugUseCase
+
+    beforeEach(()=>{
+
+        inMemoryQuestionRepository = new InMemoryQuestionRepository()
+        sut = new GetQuestionBySlugUseCase(inMemoryQuestionRepository)
+
+    })
+
+
+    it('Find question by slug', async () => {
+
+        const newQuestion = makeQuestion({
+            title: 'primeira-question'
+        })
+        inMemoryQuestionRepository.create(newQuestion)
+
+        console.log(inMemoryQuestionRepository.items)
+
+       const {question} = await sut.execute({
+            slug: 'primeira-question'
+        })
+
+        expect(question).toEqual(newQuestion)
+    
+    })
+
+})
+
+
