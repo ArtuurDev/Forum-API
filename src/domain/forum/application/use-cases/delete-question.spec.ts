11bdafca50e1d6f49b23d4a461cecd8c35fa368b
artuur <arturcastrodossantos.com@gmail.com>
2025-02-20 13:49:50 -0300

    teste unitário de delete para veficar se o caso de uso está excluindo apenas as question com ids corretos

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -2,6 +2,7 @@ import { beforeEach, describe, expect, it } from "vitest"
 import { DeleteQuestionUseCase } from "./delete-question"
 import { InMemoryQuestionRepository } from "../../../../../test/repositories/in-memory-questions-repository"
 import { makeQuestion } from "../../../../../test/factories/make-question"
+import { rejects } from "assert"
 
 describe('Delete question', () => {
     
@@ -32,5 +33,26 @@ describe('Delete question', () => {
         expect(inMemoryQuestionRepository.items).length(0)
     
     })
+
+
+    it('Should not be possible to delete a question with the wrong id ', async () => {
+
+        const newQuestion = makeQuestion({
+            title: 'primeira-question'
+        })
+        inMemoryQuestionRepository.create(newQuestion)
+
+        console.log(inMemoryQuestionRepository.items)
+
+       expect(async () => {
+        await sut.execute({
+            id: '12233'
+        })
+       }).rejects.toBeInstanceOf(Error)
+        
+            
+    })
+
+
 
 })
