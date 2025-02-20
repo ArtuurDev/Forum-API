69f287072156d39c158660e2974bb3f5022916ca
artuur <arturcastrodossantos.com@gmail.com>
2025-02-20 13:32:02 -0300

    repositorio(contrato) de question

--- src/domain/forum/application/repositories/questions-repositories.ts
+++ src/domain/forum/application/repositories/questions-repositories.ts
@@ -2,6 +2,9 @@ import { Question } from "../../enterprise/entities/question";
 
 export interface QuestionsRepository {
 
+    findById(id: string): Promise<Question | undefined>
+    delete(question:Question): Promise<void> 
+    findBySlug(slug: string): Promise<Question | null> 
     create(question: Question): Promise<void> 
 
 }
