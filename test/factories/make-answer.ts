b337012f4b9f2481bd36e6b806073e591b8b984b
artuur <arturcastrodossantos.com@gmail.com>
2025-02-21 09:20:11 -0300

    factory de answer

---
+++ test/factories/make-answer.ts
@@ -0,0 +1,16 @@
+import { faker } from "@faker-js/faker/locale/en";
+import { UniqueEntityID } from "../../src/core/entity/unique-entity-id";
+import { Answer, AnswerProps } from "../../src/domain/forum/enterprise/entities/answer";
+
+export async function makeAnswer(override: Partial<AnswerProps> = {}, id?: UniqueEntityID) {
+    
+    const answer = Answer.create({
+        authorId: new UniqueEntityID(faker.string.uuid()),
+        content: faker.lorem.text(),
+        questionId: new UniqueEntityID(faker.string.uuid()),
+        ...override
+    }, id)
+
+    return answer
+
+}
